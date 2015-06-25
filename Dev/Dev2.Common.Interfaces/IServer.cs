c1585631cdcc991c960d0864c09e9e55ffeaed93
hagashen.naidu <Hagashen.Naidu@dev2.co.za>
2015-06-25 10:22:08 +0200

    brought in the new splash screen

---
+++ Dev/Dev2.Common.Interfaces/IServer.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dev2.Common.Interfaces.Data;
+using Dev2.Common.Interfaces.Explorer;
+using Newtonsoft.Json;
+
+namespace Dev2.Common.Interfaces
+{
+    public interface IServer:IResource
+    {
+        //Task<bool> Connect();
+        //List<IResource> Load();
+       // Task<IExplorerItem> LoadExplorer();
+       // IList<IServer> GetServerConnections();
+        //IList<IToolDescriptor> LoadTools();
+        //[JsonIgnore]
+        //IExplorerRepository ExplorerRepository { get; }
+       // [JsonIgnore]
+       // IStudioUpdateManager UpdateRepository { get; }
+       // [JsonIgnore]
+       // IQueryManager QueryProxy { get; }
+      //  bool IsConnected();
+      //  void ReloadTools();
+      //  void Disconnect();
+     //   void Edit();
+        //List<IWindowsGroupPermission> Permissions { get; } 
+
+       // event PermissionsChanged PermissionsChanged;
+       // event NetworkStateChanged NetworkStateChanged;
+       // event ItemAddedEvent ItemAddedEvent;
+       
+        string GetServerVersion();
+
+       
+    }
+
+    //public delegate void PermissionsChanged(PermissionsChangedArgs args);
+   // public delegate void NetworkStateChanged(INetworkStateChangedEventArgs args);
+   // public delegate void ItemAddedEvent(IExplorerItem args);
+}
