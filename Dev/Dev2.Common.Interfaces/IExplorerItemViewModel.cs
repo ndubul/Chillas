eda5948530537eaffcb26c5869388e5660cc78f3
hagashen.naidu <Hagashen.Naidu@dev2.co.za>
2015-07-01 16:19:51 +0200

    started to add the requestservice name for the new studio

---
+++ Dev/Dev2.Common.Interfaces/IExplorerItemViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Dev2.Common.Interfaces
+{
+    public interface IExplorerItemViewModel : IExplorerTreeItem
+    {
+        
+        bool Checked { get; set; }
+        Guid ResourceId { get; set; }
+        ICommand OpenCommand { get; set; }
+        bool IsRenaming{ get; set; }
+        bool IsNotRenaming { get;  }
+        ICommand ItemSelectedCommand { get; set; }
+        ICommand LostFocus { get; set; }
+        bool IsVisible { get; set; }
+        bool AllowEditing { get; set; }
+        bool CanExecute { get; set; }
+        bool CanEdit { get; set; }
+        bool CanView { get; set; }
+        bool CanShowDependencies { get; set; }
+
+        bool IsVersion { get; set; }
+        string VersionNumber { get; set; }
+        string VersionHeader { get; set; }
+        void Filter(string filter);
+        bool Move(IExplorerItemViewModel destination);
+        bool CanDrop { get; set; }
+        bool CanDrag { get; set; }
+        ICommand OpenVersionCommand { get; set; }
+        ICommand DeleteVersionCommand { get; set; }
+        ICommand ShowDependenciesCommand { get; set; }
+        
+        string Inputs { get; set; }
+        string Outputs { get; set; }
+        string ExecuteToolTip { get; }
+        string EditToolTip { get; }
+        void AddSibling(IExplorerItemViewModel sibling);
+        void CreateNewFolder();
+        void Apply(Action<IExplorerItemViewModel> action);
+        IExplorerItemViewModel Find(string resourcePath);
+
+    }
+
+    public enum ExplorerEventContext
+    {
+        Selected
+    }
+}
---
+++ Dev/Warewolf.Studio.ViewModels/ExplorerViewModel.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Dev2.Common.Interfaces;
+using Microsoft.Practices.ObjectBuilder2;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Mvvm;
+
+namespace Warewolf.Studio.ViewModels
+{
+	public class ExplorerViewModelBase : BindableBase, IExplorerViewModel
+	{
+		private ICollection<IEnvironmentViewModel> _environments;
+		private string _searchText;
+		private bool _isRefreshing;
+		private IExplorerTreeItem _selectedItem;
+		private object[] _selectedDataItems;
+
+		public ExplorerViewModelBase()
+		{
+
+			RefreshCommand = new DelegateCommand(Refresh);
+			ClearSearchTextCommand = new DelegateCommand(() => SearchText = "");
+
+		}
+
+		public ICommand RefreshCommand { get; set; }
+
+		public bool IsRefreshing
+		{
+			get
+			{
+				return _isRefreshing;
+			}
+			set
+			{
+				_isRefreshing = value;
+				OnPropertyChanged(() => IsRefreshing);
+			}
+		}
+
+		public bool ShowConnectControl { get; set; }
+
+		public IExplorerTreeItem SelectedItem
+		{
+			get { return _selectedItem; }
+			set
+			{
+				_selectedItem = value;
+				OnPropertyChanged(() => SelectedItem);
+
+			}
+		}
+
+		public object[] SelectedDataItems
+		{
+			get { return _selectedDataItems; }
+			set
+			{
+				_selectedDataItems = value;
+				OnPropertyChanged(() => SelectedDataItems);
+			}
+		}
+
+		public ICollection<IEnvironmentViewModel> Environments
+		{
+			get
+			{
+				return _environments;
+			}
+			set
+			{
+				_environments = value;
+				OnPropertyChanged(() => Environments);
+			}
+		}
+
+		public IEnvironmentViewModel SelectedEnvironment { get; set; }
+		public IServer SelectedServer { get { return SelectedEnvironment.Server; } }
+
+
+		public string SearchText
+		{
+			get
+			{
+				return _searchText;
+			}
+			set
+			{
+				if (_searchText == value)
+				{
+					return;
+				}
+				_searchText = value;
+				Filter(_searchText);
+				OnPropertyChanged(() => SearchText);
+			}
+		}
+
+		public string SearchToolTip
+		{
+			get
+			{
+				return Resources.Languages.Core.ExplorerSearchToolTip;
+			}
+		}
+
+		public string RefreshToolTip
+		{
+			get
+			{
+				return Resources.Languages.Core.ExplorerRefreshToolTip;
+			}
+		}
+
+		private void Refresh()
+		{
+			IsRefreshing = true;
+			Environments.ForEach(model =>
+			{
+				if (model.IsConnected)
+				{
+					model.Load();
+				}
+			});
+			IsRefreshing = false;
+		}
+
+		public void Filter(string filter)
+		{
+			if (Environments != null)
+			{
+				foreach (var environmentViewModel in Environments)
+				{
+					environmentViewModel.Filter(filter);
+				}
+				OnPropertyChanged(() => Environments);
+			}
+		}
+
+		public void RemoveItem(IExplorerItemViewModel item)
+		{
+			if (Environments != null)
+			{
+				var env = Environments.FirstOrDefault(a => a.Server == item.Server);
+
+				if (env != null)
+				{
+					if (env.Children.Contains(item))
+					{
+						env.RemoveChild(item);
+					}
+					else
+						env.RemoveItem(item);
+				}
+				OnPropertyChanged(() => Environments);
+			}
+		}
+
+		public event SelectedExplorerEnvironmentChanged SelectedEnvironmentChanged;
+
+
+		public ICommand ClearSearchTextCommand { get; private set; }
+
+		public void SelectItem(Guid id)
+		{
+			foreach (var environmentViewModel in Environments)
+			{
+				environmentViewModel.SelectItem(id, (a => SelectedItem = a));
+			}
+		}
+
+		public IList<IExplorerItemViewModel> FindItems(Func<IExplorerItemViewModel, bool> filterFunc)
+		{
+			return null;
+		}
+		//public IConnectControlViewModel ConnectControlViewModel { get; internal set; }
+		protected virtual void OnSelectedEnvironmentChanged(IEnvironmentViewModel e)
+		{
+			var handler = SelectedEnvironmentChanged;
+			if (handler != null) handler(this, e);
+		}
+	}
+
+//	public class ExplorerViewModel : ExplorerViewModelBase
+//	{
+//		public ExplorerViewModel(IShellViewModel shellViewModel, IEventAggregator aggregator)
+//		{
+//			if (shellViewModel == null)
+//			{
+//				throw new ArgumentNullException("shellViewModel");
+//			}
+//			var localhostEnvironment = CreateEnvironmentFromServer(shellViewModel.LocalhostServer, shellViewModel);
+//			Environments = new ObservableCollection<IEnvironmentViewModel> { localhostEnvironment };
+//			LoadEnvironment(localhostEnvironment);
+//
+//			ConnectControlViewModel = new ConnectControlViewModel(shellViewModel.LocalhostServer, aggregator);
+//			ShowConnectControl = true;
+//			aggregator.GetEvent<ItemAddedEvent>().Subscribe(ItemAdded);
+//		}
+//
+//		void ItemAdded(IExplorerItem obj)
+//		{
+//
+//		}
+//
+//		private async void LoadEnvironment(IEnvironmentViewModel localhostEnvironment)
+//		{
+//			await localhostEnvironment.Connect();
+//			await localhostEnvironment.Load();
+//		}
+//
+//
+//		IEnvironmentViewModel CreateEnvironmentFromServer(IServer server, IShellViewModel shellViewModel)
+//		{
+//			return new EnvironmentViewModel(server, shellViewModel);
+//		}
+//	}
+
+	public class SingleEnvironmentExplorerViewModel : ExplorerViewModelBase
+	{
+		public SingleEnvironmentExplorerViewModel(IEnvironmentViewModel environmentViewModel)
+		{
+			environmentViewModel.SetPropertiesForDialog();
+			Environments = new ObservableCollection<IEnvironmentViewModel>
+			{
+				environmentViewModel
+			};
+			ShowConnectControl = false;
+		}
+	}
+}
