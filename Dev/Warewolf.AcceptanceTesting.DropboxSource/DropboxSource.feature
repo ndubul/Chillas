0c7dd081b0d11089e44c1c480c3dca7c7057beed
Leroy <Leroy.warner@Dev2.co.za>
2015-08-07 13:34:27 +0200

    Added DropBox specs for audit

--- Dev/Warewolf.AcceptanceTesting.DropboxSource/DropboxSource.feature
+++ Dev/Warewolf.AcceptanceTesting.DropboxSource/DropboxSource.feature
@@ -6,6 +6,50 @@
 @mytag
 Scenario: New Dropbox Source
 	Given I have open New Dropbox Source
-	And I have entered 70 into the calculator
-	When I press add
-	Then the result should be 120 on the screen
+	Then "New Dropbox Source" tab is opened
+	And title is "New Dropbox Source"
+	Then "Email" has the focus
+	And "Email" is ""
+	And "Password" is ""
+	And "Forgot your Password" is visible
+	And "New to Dropbox?Create an account" is visible
+	And "Log in" is enabled
+
+Scenario: Create a new Dropbox Source
+	Given I have open New Dropbox Source
+	Then "New Dropbox Source" tab is opened
+	And title is "New Dropbox Source"
+	Then "Email" has the focus
+	And "Email" is "Someone@dev2.co.za"
+	And "Password" is "•••••••"
+	And I click "Log In"
+	Then "Warewolf ESB would like access to the files and folders in your Dropbox" is visible
+	And "Cancel" is enabled
+	And "Allow" is enabled
+	When I click "Allow"
+	Then the save dialog is opened
+	And I save as "MyDropboxSource"
+	Then "MyDropboxSource is visible in the Explorer
+
+Scenario: Denying Warewolf access to Dropbox
+	Given I have open New Dropbox Source
+	Then "New Dropbox Source" tab is opened
+	And title is "New Dropbox Source"
+	Then "Email" has the focus
+	And "Email" is "Someone@dev2.co.za"
+	And "Password" is "•••••••"
+	And I click "Log In"
+	Then "Warewolf ESB would like access to the files and folders in your Dropbox" is visible
+	And "Cancel" is enabled
+	And "Allow" is enabled
+	When I click "Cancel"
+	Then "New Dropbox Source" tab is closed
+
+Scenario: Edit saved Dropbox Source
+	Given I click "Edit - Dropbox Source"
+	Then "Edit - Dropbox Source" tab is opened
+	And title is "Edit - Dropbox Source"
+	And "Email" has the focus
+	And "Email" is ""
+	And "Password" is ""
+
--- Dev/Warewolf.AcceptanceTesting.DropboxSource/DropboxSource.feature.cs
+++ Dev/Warewolf.AcceptanceTesting.DropboxSource/DropboxSource.feature.cs
@@ -82,11 +82,123 @@ this.ScenarioSetup(scenarioInfo);
 #line 8
  testRunner.Given("I have open New Dropbox Source", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
 #line 9
- testRunner.And("I have entered 70 into the calculator", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+ testRunner.Then("\"New Dropbox Source\" tab is opened", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line 10
- testRunner.When("I press add", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+ testRunner.And("title is \"New Dropbox Source\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line 11
- testRunner.Then("the result should be 120 on the screen", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+ testRunner.Then("\"Email\" has the focus", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line 12
+ testRunner.And("\"Email\" is \"\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 13
+ testRunner.And("\"Password\" is \"\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 14
+ testRunner.And("\"Forgot your Password\" is visible", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 15
+ testRunner.And("\"New to Dropbox?Create an account\" is visible", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 16
+ testRunner.And("\"Log in\" is enabled", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+            this.ScenarioCleanup();
+        }
+        
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute()]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.DescriptionAttribute("Create a new Dropbox Source")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("FeatureTitle", "DropboxSource")]
+        public virtual void CreateANewDropboxSource()
+        {
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Create a new Dropbox Source", ((string[])(null)));
+#line 18
+this.ScenarioSetup(scenarioInfo);
+#line 19
+ testRunner.Given("I have open New Dropbox Source", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line 20
+ testRunner.Then("\"New Dropbox Source\" tab is opened", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line 21
+ testRunner.And("title is \"New Dropbox Source\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 22
+ testRunner.Then("\"Email\" has the focus", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line 23
+ testRunner.And("\"Email\" is \"Someone@dev2.co.za\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 24
+ testRunner.And("\"Password\" is \"•••••••\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 25
+ testRunner.And("I click \"Log In\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 26
+ testRunner.Then("\"Warewolf ESB would like access to the files and folders in your Dropbox\" is visi" +
+                    "ble", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line 27
+ testRunner.And("\"Cancel\" is enabled", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 28
+ testRunner.And("\"Allow\" is enabled", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 29
+ testRunner.When("I click \"Allow\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 30
+ testRunner.Then("the save dialog is opened", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line 31
+ testRunner.And("I save as \"MyDropboxSource\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 32
+ testRunner.Then("\"MyDropboxSource is visible in the Explorer", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+            this.ScenarioCleanup();
+        }
+        
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute()]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.DescriptionAttribute("Denying Warewolf access to Dropbox")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("FeatureTitle", "DropboxSource")]
+        public virtual void DenyingWarewolfAccessToDropbox()
+        {
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Denying Warewolf access to Dropbox", ((string[])(null)));
+#line 34
+this.ScenarioSetup(scenarioInfo);
+#line 35
+ testRunner.Given("I have open New Dropbox Source", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line 36
+ testRunner.Then("\"New Dropbox Source\" tab is opened", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line 37
+ testRunner.And("title is \"New Dropbox Source\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 38
+ testRunner.Then("\"Email\" has the focus", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line 39
+ testRunner.And("\"Email\" is \"Someone@dev2.co.za\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 40
+ testRunner.And("\"Password\" is \"•••••••\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 41
+ testRunner.And("I click \"Log In\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 42
+ testRunner.Then("\"Warewolf ESB would like access to the files and folders in your Dropbox\" is visi" +
+                    "ble", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line 43
+ testRunner.And("\"Cancel\" is enabled", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 44
+ testRunner.And("\"Allow\" is enabled", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 45
+ testRunner.When("I click \"Cancel\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 46
+ testRunner.Then("\"New Dropbox Source\" tab is closed", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+            this.ScenarioCleanup();
+        }
+        
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute()]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.DescriptionAttribute("Edit saved Dropbox Source")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("FeatureTitle", "DropboxSource")]
+        public virtual void EditSavedDropboxSource()
+        {
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Edit saved Dropbox Source", ((string[])(null)));
+#line 48
+this.ScenarioSetup(scenarioInfo);
+#line 49
+ testRunner.Given("I click \"Edit - Dropbox Source\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line 50
+ testRunner.Then("\"Edit - Dropbox Source\" tab is opened", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line 51
+ testRunner.And("title is \"Edit - Dropbox Source\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 52
+ testRunner.And("\"Email\" has the focus", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 53
+ testRunner.And("\"Email\" is \"\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 54
+ testRunner.And("\"Password\" is \"\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
             this.ScenarioCleanup();
         }
