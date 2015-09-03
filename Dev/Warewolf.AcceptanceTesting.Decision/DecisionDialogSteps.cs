d6899133e71d6ed429aacc7d8e482e8e3141b786
Pieter Terblanche <Pieter.Terblanche@dev2.co.za>
2015-09-03 15:09:25 +0200

    1082 decision tests

--- Dev/Warewolf.AcceptanceTesting.Decision/DecisionDialogSteps.cs
+++ Dev/Warewolf.AcceptanceTesting.Decision/DecisionDialogSteps.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Dev2.Activities.Designers2.Decision;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TechTalk.SpecFlow;
+using Warewolf.AcceptanceTesting.Core;
 
 namespace Warewolf.AcceptanceTesting.Decision
 {
@@ -7,195 +9,210 @@ namespace Warewolf.AcceptanceTesting.Decision
     public class DecisionDialogSteps
     {
         [Given(@"I have a workflow ""(.*)""")]
-public void GivenIHaveAWorkflow(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void GivenIHaveAWorkflow(string p0)
+        {
+            var sourceControl = ScenarioContext.Current.Get<DecisionDesigner>(Utils.ViewNameKey);
+            Assert.IsNotNull(sourceControl);
+            Assert.IsNotNull(sourceControl.DataContext); 
+        }
 
         [Given(@"drop a ""(.*)"" tool onto the design surface")]
-public void GivenDropAToolOntoTheDesignSurface(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void GivenDropAToolOntoTheDesignSurface(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Given(@"the decision tool window is opened")]
-public void GivenTheDecisionToolWindowIsOpened()
-{
-    ScenarioContext.Current.Pending();
-}
+        public void GivenTheDecisionToolWindowIsOpened()
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Given(@"a decision variable ""(.*)"" value ""(.*)""")]
-public void GivenADecisionVariableValue(string p0, string p1)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void GivenADecisionVariableValue(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Given(@"is ""(.*)"" ""(.*)"" ""(.*)""")]
-public void GivenIs(string p0, string p1, string p2)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void GivenIs(string p0, string p1, string p2)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Given(@"""(.*)"" is selected")]
-public void GivenIsSelected(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void GivenIsSelected(string name)
+        {
+            var view = Utils.GetView<DecisionDesigner>();
+            view.DoneAction();
+        }
 
         [Given(@"a decision variable ""(.*)"" value ""(.*)""")]
-public void GivenADecisionVariableValue(string p0, int p1)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void GivenADecisionVariableValue(string p0, int p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Given(@"""(.*)"" is ""(.*)""")]
-public void GivenIs(string p0, string p1)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void GivenIs(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Given(@"Match Type equals ""(.*)""")]
-public void GivenMatchTypeEquals(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void GivenMatchTypeEquals(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Given(@"I right click to view the context menu")]
-public void GivenIRightClickToViewTheContextMenu()
-{
-    ScenarioContext.Current.Pending();
-}
+        public void GivenIRightClickToViewTheContextMenu()
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Given(@"I select ""(.*)""")]
-public void GivenISelect(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void GivenISelect(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Given(@"""(.*)"" ""(.*)"" ""(.*)"" is removed from the decision")]
-public void GivenIsRemovedFromTheDecision(string p0, string p1, string p2)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void GivenIsRemovedFromTheDecision(string p0, string p1, string p2)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Given(@"the decision has ""(.*)"" error")]
-public void GivenTheDecisionHasError(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void GivenTheDecisionHasError(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Given(@"the decision tool has ""(.*)"" Error")]
-public void GivenTheDecisionToolHasError(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void GivenTheDecisionToolHasError(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Given(@"Error message ""(.*)""\[\[A]]}"" is visible")]
-public void GivenErrorMessageAIsVisible(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void GivenErrorMessageAIsVisible(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Given(@"I select the ""(.*)"" menu")]
-public void GivenISelectTheMenu(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void GivenISelectTheMenu(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Given(@"Match Type has '(.*)' visible")]
-public void GivenMatchTypeHasVisible(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void GivenMatchTypeHasVisible(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [When(@"the decision tool is executed")]
-public void WhenTheDecisionToolIsExecuted()
-{
-    ScenarioContext.Current.Pending();
-}
+        public void WhenTheDecisionToolIsExecuted()
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [When(@"I change decision variable ""(.*)"" to ""(.*)""")]
-public void WhenIChangeDecisionVariableTo(string p0, int p1)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void WhenIChangeDecisionVariableTo(string p0, int p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [When(@"""(.*)"" is selected")]
-public void WhenIsSelected(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void WhenIsSelected(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Then(@"the Decision window is opened")]
-public void ThenTheDecisionWindowIsOpened()
-{
-    ScenarioContext.Current.Pending();
-}
+        [Given(@"the Decision window is opened")]
+        public void ThenTheDecisionWindowIsOpened()
+        {
+            var viewModel = Utils.GetViewModel<DecisionDesignerViewModel>();
+            Assert.IsTrue(viewModel.ShowLarge);
+        }
 
         [Then(@"'(.*)' fields are ""(.*)""")]
-public void ThenFieldsAre(string p0, string p1)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void ThenFieldsAre(string name, string state)
+        {
+            var view = Utils.GetView<DecisionDesigner>();
+            switch (name)
+            {
+                case "<Inputs>":
+                    Utils.CheckControlEnabled(name, state, view);
+                    break;
+                default:
+                    Utils.CheckControlEnabled(name, state, view);
+                    break;
+
+            }
+        }
 
         [Then(@"""(.*)"" is visible")]
-public void ThenIsVisible(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void ThenIsVisible(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Then(@"a decision variable '(.*)' value '(.*)'")]
-public void ThenADecisionVariableValue(string p0, string p1)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void ThenADecisionVariableValue(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Then(@"check if '(.*)' ""(.*)"" '(.*)'")]
-public void ThenCheckIf(string p0, string p1, string p2)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void ThenCheckIf(string p0, string p1, string p2)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Then(@"the execution has ""(.*)"" error")]
-public void ThenTheExecutionHasError(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void ThenTheExecutionHasError(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Then(@"the Decision tool window is closed")]
-public void ThenTheDecisionToolWindowIsClosed()
-{
-    ScenarioContext.Current.Pending();
-}
+        public void ThenTheDecisionToolWindowIsClosed()
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Then(@"""(.*)"" is visible in tool")]
-public void ThenIsVisibleInTool(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void ThenIsVisibleInTool(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Then(@"I open the Decision tool window")]
-public void ThenIOpenTheDecisionToolWindow()
-{
-    ScenarioContext.Current.Pending();
-}
+        public void ThenIOpenTheDecisionToolWindow()
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Then(@"decision variable ""(.*)"" is not visible")]
-public void ThenDecisionVariableIsNotVisible(string p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void ThenDecisionVariableIsNotVisible(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Then(@"""(.*)"" is visible in Match field")]
-public void ThenIsVisibleInMatchField(int p0)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void ThenIsVisibleInMatchField(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
 
         [Then(@"""(.*)"" is ""(.*)""")]
-public void ThenIs(string p0, string p1)
-{
-    ScenarioContext.Current.Pending();
-}
+        public void ThenIs(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
     }
 }
