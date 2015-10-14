5daedd35866f7508ba56577dddc0e4f4c607698a
Pieter Terblanche <Pieter.Terblanche@dev2.co.za>
2015-10-14 10:22:21 +0200

    #WOLF-358 Add checkbox option to environment

--- Dev/Warewolf.Studio.ViewModels/DeployViewModel.cs
+++ Dev/Warewolf.Studio.ViewModels/DeployViewModel.cs
@@ -3,7 +3,6 @@ using System.Windows.Input;
 using Dev2;
 using Dev2.Common.Interfaces;
 using Dev2.Common.Interfaces.Deploy;
-using Dev2.Studio.Core.Interfaces;
 using Microsoft.Practices.Prism.Commands;
 using Microsoft.Practices.Prism.Mvvm;
 
