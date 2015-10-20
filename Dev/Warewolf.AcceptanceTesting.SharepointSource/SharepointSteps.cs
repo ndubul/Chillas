68ef100ecfbab2c63d6c4acb62dbc783e70e77ca
Pieter Terblanche <Pieter.Terblanche@dev2.co.za>
2015-10-20 16:00:12 +0200

    #WOLF-1161 EmailSource tests

---
+++ Dev/Warewolf.AcceptanceTesting.SharepointSource/SharepointSteps.cs
@@ -0,0 +1,218 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Warewolf.AcceptanceTesting.SharepointSource
+{
+    [Binding]
+    public class SharepointSteps
+    {
+        [Given(@"I open New Sharepoint Source")]
+        public void GivenIOpenNewSharepointSource()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I type Address as ""(.*)""")]
+        public void GivenITypeAddressAs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"""(.*)"" is ""(.*)""")]
+        public void GivenIs(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I Select Authentication Type as ""(.*)""")]
+        public void GivenISelectAuthenticationTypeAs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"Username field is ""(.*)""")]
+        public void GivenUsernameFieldIs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"Password field is ""(.*)""")]
+        public void GivenPasswordFieldIs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I type Username as ""(.*)""")]
+        public void GivenITypeUsernameAs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I type Password as ""(.*)""")]
+        public void GivenITypePasswordAs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I open ""(.*)"" Sharepoint source")]
+        public void GivenIOpenSharepointSource(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Test Connecton is ""(.*)""")]
+        public void WhenTestConnectonIs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"""(.*)"" is ""(.*)""")]
+        public void WhenIs(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I save as ""(.*)""")]
+        public void WhenISaveAs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I save the source")]
+        public void WhenISaveTheSource()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Validation message is thrown")]
+        public void WhenValidationMessageIsThrown()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Validation message is Not thrown")]
+        public void WhenValidationMessageIsNotThrown()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I Select Authentication Type as ""(.*)""")]
+        public void WhenISelectAuthenticationTypeAs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Username field is ""(.*)""")]
+        public void WhenUsernameFieldIs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Password field is ""(.*)""")]
+        public void WhenPasswordFieldIs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I change Address to ""(.*)""")]
+        public void WhenIChangeAddressTo(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I edit Authentication Type as ""(.*)""")]
+        public void WhenIEditAuthenticationTypeAs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Username field as ""(.*)""")]
+        public void WhenUsernameFieldAs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Password field as ""(.*)""")]
+        public void WhenPasswordFieldAs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I Cancel the Test")]
+        public void WhenICancelTheTest()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"""(.*)"" tab is opened")]
+        public void ThenTabIsOpened(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"title is ""(.*)""")]
+        public void ThenTitleIs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I type Address as ""(.*)""")]
+        public void ThenITypeAddressAs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"""(.*)"" is ""(.*)""")]
+        public void ThenIs(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I Select Authentication Type as ""(.*)""")]
+        public void ThenISelectAuthenticationTypeAs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Username field is ""(.*)""")]
+        public void ThenUsernameFieldIs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Password field is ""(.*)""")]
+        public void ThenPasswordFieldIs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the save dialog is opened")]
+        public void ThenTheSaveDialogIsOpened()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Address is ""(.*)""")]
+        public void ThenAddressIs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Select Authentication Type as ""(.*)""")]
+        public void ThenSelectAuthenticationTypeAs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Validation message is thrown")]
+        public void ThenValidationMessageIsThrown()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Validation message is ""(.*)""")]
+        public void ThenValidationMessageIs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
